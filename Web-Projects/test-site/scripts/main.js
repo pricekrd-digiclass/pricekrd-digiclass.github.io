30294c7574307a8518585ea675346f432ac9447e
pricekrd-digiclass <44858816+pricekrd-digiclass@users.noreply.github.com>
2018-11-19 23:18:34 -0500

    Add files via upload

---
+++ Web-Projects/test-site/scripts/main.js
@@ -0,0 +1,6 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'The Adventures of Milo & Data';
+//This is my comment
+document.querySelector('html').onclick = function() {
+    alert('Meow! Stop poking me!');
+}
